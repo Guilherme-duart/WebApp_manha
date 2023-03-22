0816bedd2d4b4e9e0cc7fc4027f73b9b71b9b913
Guilherme-duart <duarteclebio@gmail.com>
2023-03-22 10:07:23 -0300

    Criando novas classes

--- WebApp_manha/Controllers/HomeController.cs
+++ WebApp_manha/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace WebApp_manha.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
