0dbcef72361e6f414447dddf98e175fa60c43dbd
Guilherme-duart <duarteclebio@gmail.com>
2023-04-19 10:41:45 -0300

    Aplicação do método Model do MVC

--- WebApp_manha/Controllers/ClientesController.cs
+++ WebApp_manha/Controllers/ClientesController.cs
@@ -1,18 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp_manha.Models;
 
 namespace WebApp_manha.Controllers
 {
     public class ClientesController : Controller
     {
+      public static List<ClienteViewModel> lista = new List<ClienteViewModel>();
         public IActionResult Lista()
         {
-            return View();
+            //ClienteViewModel novo = new ClienteViewModel();
+           
+            //for (var i = 0; i <10; i++)
+            //{
+            //    novo.Id = 10;
+            //    novo.Nome = "Guilherme Duarte";
+            //    novo.Telefone = "(16)34567890";
+
+            //    lista.Add(novo);
+
+            //}
+
+        
+            return View(lista);
         }
 
         public IActionResult Cadastro()
         {
             return View();
+          
         }
+        [HttpPost]
+        public IActionResult SalvarDados(ClienteViewModel model)
+        {
+            lista.Add(model);
+            return RedirectToAction("Lista");
+        }
+
         [HttpPost]
         public IActionResult Cadastro(string Nome, string Telefone)
         {
@@ -35,7 +58,12 @@ namespace WebApp_manha.Controllers
 
         public IActionResult Excluir(int id)
         {
-            return View();
+            ClienteViewModel cliente = lista.Find(a => a.Id == id); 
+            if (cliente != null)
+            {
+                lista.Remove(cliente);
+            }
+            return RedirectToAction("Lista");
         }
 
 
@@ -43,6 +71,27 @@ namespace WebApp_manha.Controllers
     }
 }
 
+
+
+
+//public IActionResult Lista()
+//{
+//    ClienteViewModel novo = new ClienteViewModel();
+//    novo.Nome = "Fernando Graciano";
+//    novo.Id = 10;
+//    novo.Telefone = "16991340447";
+
+//    ClienteViewModel novo2 = new ClienteViewModel();
+//    novo.Nome = "Julio Garcia";
+//    novo.Id = 5;
+//    novo.Telefone = "169874563214";
+
+//    List<ClienteViewModel> lista = new List<ClienteViewModel>();
+//    lista.Add(novo);
+//    lista.Add(novo2);
+
+//    return View(lista);
+//}
 
 
 
